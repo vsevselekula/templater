59229092b79d45483af758c24ae1e076562e57dc
Вячеслав Петров <vselekula@icloud.com>
2021-05-06 12:00:49 +0300

    main trigger is working, next is gallery

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,33 +1,125 @@
+
 <template>
   <Layout>
+<div class="mainContent" >
+  <div class="fullPreview" id="capture">
+    <div 
+    style="display: table-cell; vertical-align: middle"
+    contenteditable="true" class="picMessage">
+    I am vertically aligned
+  </div>
+    <!-- <textarea class="picMessage" type="text" placeholder="Place your fu*cking text here"/> -->
+  </div>
+  <div class="thumbnails">
+    <div class="thumb">
 
-    <!-- Learn how to use images here: https://gridsome.org/docs/images -->
-    <g-image alt="Example image" src="~/favicon.png" width="135" />
-
-    <h1>Hello, world!</h1>
-
-    <p>
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur excepturi labore tempore expedita, et iste tenetur suscipit explicabo! Dolores, aperiam non officia eos quod asperiores
-    </p>
-
-    <p class="home-links">
-      <a href="https://gridsome.org/docs/" target="_blank" rel="noopener">Gridsome Docs</a>
-      <a href="https://github.com/gridsome/gridsome" target="_blank" rel="noopener">GitHub</a>
-    </p>
+    </div>
+  </div>
+  <div class="controls">
+    <button v-on:click="renderPic">скачать
+    
+    </button>
+  </div>
+</div>
 
   </Layout>
 </template>
 
 <script>
+import html2canvas from 'html2canvas'
+
 export default {
-  metaInfo: {
-    title: 'Hello, world!'
+  
+  methods: {
+//     renderPic: function (event) {
+//      html2canvas(document.querySelector("#capture"), {scale: 2}).then(i => {
+//     document.body.appendChild(i);
+//     var image = i.toDataURL("image/png")
+//     .replace("image/png", "image/octet-stream");
+// // download.setAttribute("href", image);
+//     })
+//   }
+  renderPic: function (event) {
+     html2canvas(document.querySelector("#capture"), {scale: 2}).then(i => {
+    saveAs(i.toDataURL(), 'file-name.png');
+    })
+    function saveAs(uri, filename) {
+
+    var link = document.createElement('a');
+
+    if (typeof link.download === 'string') {
+
+        link.href = uri;
+        link.download = filename;
+
+        //Firefox requires the link to be in the body
+        document.body.appendChild(link);
+
+        //simulate click
+        link.click();
+
+        //remove the link when done
+        document.body.removeChild(link);
+
+    } else {
+
+        window.open(uri);
+
+    }
+}
+  }
   }
 }
 </script>
 
 <style>
-.home-links a {
-  margin-right: 1rem;
+.mainContent {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    flex-direction: column;
+        align-items: center;
+
 }
+.picMessage{
+  background: none;
+    border: none;
+    width: 100%;    
+    white-space: normal;
+    text-align-last: center;
+    font-size: 42px;
+    font-family: Lato;
+    font-weight: bold;
+}
+.fullPreview {
+    width: 500px;
+    height: 500px;
+    display: flex;
+    
+    align-items: center;
+    background-color: wheat;
+}
+</style>
+<style>
+  @font-face {
+    font-family: Lato;
+    src: url('../assets/Lato-Regular.ttf');
+    font-weight: normal;
+  }
+  @font-face {
+    font-family: Lato;
+    src: url('../assets/Lato-Bold.ttf');
+    font-weight: Bold;
+  }
+  @font-face {
+    font-family: Euclid;
+    src: url('../assets/EuclidCircularB-Bold.otf');
+    font-weight: bold;
+  }
+  @font-face {
+    font-family: Euclid;
+    src: url('../assets/EuclidCircularB-Regular.otf');
+    font-weight: normal;
+  }
 </style>
